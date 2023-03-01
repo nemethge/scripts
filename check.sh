370f8bd50e42424ff602077922fb69e5250526c2
nemethge <nemethge@gmail.com>
2023-03-01 08:36:07 +0100

    Create check.sh

---
+++ check.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# List running services
+echo "Running services:"
+systemctl list-units --type=service --state=running
+
+# List running containers
+echo "Running containers:"
+docker ps
+
+# List compose files
+echo "Compose files:"
+find . -name docker-compose.yml -type f
