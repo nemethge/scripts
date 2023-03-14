071f7ac2eb238d2a1f0376c1991d2c1c32a54347
nemethge <nemethge@gmail.com>
2023-03-14 10:00:04 +0100

    Create dockerinstall.sh

---
+++ dockerinstall.sh
@@ -0,0 +1,5 @@
+sudo apt -y install apt-transport-https ca-certificates curl gnupg2 software-properties-common
+curl -fsSL https://download.docker.com/linux/debian/gpg | gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+echo "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/debian $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list
+apt-get update
+apt-get install docker-ce docker-ce-cli containerd.io
