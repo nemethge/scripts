02934f5ab5c14a534ef9e1c49e56a2cf320cbb0c
nemethge <nemethge@gmail.com>
2023-06-07 13:34:38 +0200

    create.sh

--- create.sh
+++ create.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
-# Define source VM ID and target VM names
+# Define source VM ID
 src_vmid=100
-declare -a vm_names=("node1" "node2")
 
 # Get maximum existing VM ID
 max_vmid=$(qm list | awk 'NR>1 {print $1}' | sort -nr | head -n 1)
@@ -12,11 +11,23 @@ if [[ -z "$max_vmid" ]]; then
   max_vmid=100
 fi
 
-# Loop over vm_names
-for vm_name in "${vm_names[@]}"
+# Ask for the number of nodes to create
+read -p "How many nodes do you need to create? " node_count
+
+# Check if the input is a number
+if ! [[ "$node_count" =~ ^[0-9]+$ ]]; then
+   echo "Invalid input! Please enter a number."
+   exit 1
+fi
+
+# Loop to create the specified number of nodes
+for (( i=1; i<=node_count; i++ ))
 do
     # Increment max_vmid to get the next available VM ID
     ((max_vmid++))
+
+    # Generate a VM name
+    vm_name="node$i"
 
     # Clone the VM
     qm clone $src_vmid $max_vmid --name $vm_name
