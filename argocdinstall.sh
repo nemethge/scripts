f3910ae066cecea80fdd5c58c7aa284b05d90a61
nemethge <nemethge@gmail.com>
2023-10-20 11:38:48 +0200

    Create argocdinstall.sh

---
+++ argocdinstall.sh
@@ -0,0 +1,2 @@
+kubectl create namespace argocd && kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
+kubectl -n argocd get secret argocd-initial-admin-secret -o yaml -o jsonpath="{.data.password}" | base64 -d
